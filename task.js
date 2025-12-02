5086c2d5cbfb23345aebba626e63ce28c2cd6a62
zizo elbadawy <abdalaziz102277@icloud.com>
2025-12-02 21:09:13 +0200

    first commit

---
+++ task.js
@@ -0,0 +1,48 @@
+let job = "Manager";
+let salary = 0;
+
+switch (job) {
+    case "Manager":
+        salary = 8000;
+        break;
+
+    case "IT":
+    case "Support":
+        salary = 6000;
+        break;
+
+    case "Developer":
+    case "Designer":
+        salary = 7000;
+        break;
+
+    default:
+        salary = 4000;
+}
+console.log(salary);
+
+
+//if challenge
+
+// If Challenge
+
+let holidays = 3;
+let money = 0;
+if (holidays === 0) {
+  money = 5000;
+} else if (holidays === 1 || holidays === 2) {
+  money = 3000;
+} else if (holidays === 3) {
+  money = 2000;
+} else if (holidays === 4) {
+  money = 1000;
+} else if (holidays === 5) {
+  money = 0;
+} else {
+  money = 0;
+}
+
+console.log("My Money Is ${money}"); 
+
+console.log(`My Money Is ${money}`); 
+
