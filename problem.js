f485a85a659e994ff24d9aaeb3dd2a52823075a3
zizo elbadawy <abdalaziz102277@icloud.com>
2025-12-03 18:24:09 +0200

    Add initial implementation to count and log numbers greater than or equal to 10

---
+++ problem.js
@@ -0,0 +1,15 @@
+let nums = [4 , 9 , 13 , 8 , 20];
+
+let count = 0;
+
+for (let i = 0; i < nums.length; i++){
+
+if (nums[i]>=10 ) {
+
+    count++;
+
+    console.log(`number: ${nums[i]}`);
+}
+
+
+}
